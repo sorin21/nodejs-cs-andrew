ae5a6971e39896dc7232fb1c00342e92da9b7f4e
Cristina Dragomir <cristina.terente05@gmail.com>
2019-10-21 08:05:04 +0300

    Middle of addNote 19-8:27

--- 04.args/notes/app.js
+++ 04.args/notes/app.js
@@ -2,7 +2,7 @@ const validator = require('validator');
 const chalk = require('chalk');
 const yargs = require('yargs');
 
-const getNotes = require('./notes.js');
+const notes = require('./notes.js');
 // argument vector. an array with all arguments provided
 // console.log(process.argv[2])
 // const command = process.argv[2];
@@ -19,23 +19,42 @@ yargs.version('1.0.1')
 yargs.command({
   command: 'add',
   describe: 'Add a new note',
-  handler: function() {
-    console.log('Add a new note');
+  builder: {
+    title: {
+      describe: 'Note title ',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function (argv) {
+    // console.log(`Title: ${argv.title}`);
+    // console.log(`Body: ${argv.body}`);
+    notes.addNote(argv.title, argv.body)
   }
 })
 
 yargs.command({
   command: 'remove',
   describe: 'Remove a note',
-  handler: function() {
-    console.log('Remove a note');
+  builder: {
+    title: {
+      describe: 'Note title '
+    }
+  },
+  handler: function (argv) {
+    console.log('Remove a note', argv);
   }
 })
 
 yargs.command({
   command: 'read',
   describe: 'Read a note',
-  handler: function() {
+  handler: function () {
     console.log('Read a note');
   }
 })
@@ -43,9 +62,12 @@ yargs.command({
 yargs.command({
   command: 'list',
   describe: 'List all notes',
-  handler: function() {
+  handler: function () {
     console.log('List all notes');
   }
 })
 
-console.log(yargs.argv)
+// console.log(yargs.argv)
+
+// instead of console yargs.argv
+yargs.parse();
--- 04.args/notes/notes.js
+++ 04.args/notes/notes.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 // const getNotes = () => {
 //   const fs = require('fs');
 //   // takes 2 arguments, both strings
@@ -11,4 +13,22 @@ const getNotes = function () {
   return 'Your notes...'
 }
 
-module.exports = getNotes
+const addNote = function (title, body) {
+  const notes = loadNotes();
+  console.log(notes)
+}
+
+const loadNotes = function () {
+  // we use try catch block to not fail te loadNotes function
+  // because 1st time when we load it notes.json does not exist
+  try {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (error) {
+    return []
+  }
+
+}
+
+module.exports = { getNotes, addNote }
